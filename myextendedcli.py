e88c560ac82e5ec8037261e2bd9e321c7ef8f983
Konstantin Molchanov <moigagoo@live.com>
2015-08-26 11:50:54 +0300

    Example updated.

--- myextendedcli.py
+++ myextendedcli.py
@@ -1,8 +1,10 @@
+﻿from cliar import add_aliases
 from mycli import MyCLI
 
 class MyExtendedCLI(MyCLI):
     '''This is the extended version of my command-line interface'''
 
+    @add_aliases(['gb'])
     def goodbye(self):
         print('Good bye, and happy hacking!')
 
