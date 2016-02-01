d37cb2c43aef3c6a9300b27ce77bd679ad555a74
Konstantin Molchanov <moigagoo@live.com>
2016-02-01 15:30:04 +0300

    Python 2/3 sources renamed to legacy/modern.

--- cliar/__init__.py
+++ cliar/__init__.py
@@ -1,8 +1,11 @@
 from sys import version_info
 
 
-if version_info.major == 3:
-    from .py3.cliar import *
+if version_info.major < 3:
+    from .legacy.cliar import *
 
-elif version_info.major == 2:
-    from .py2.cliar import *
+elif version_info.major == 3 and version_info.minor < 3:
+    raise NotImplementedError('Python 3.1 and 3.2 is not supported.')
+
+else:
+    from .modern.cliar import *
