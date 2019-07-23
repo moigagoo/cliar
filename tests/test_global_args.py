7a0d18609d59fb17b454a542c242890c47980cb5
Constantine Molchanov <moigagoo@live.com>
2019-07-23 10:21:33 +0400

    Fix test and add new one.

--- tests/test_global_args.py
+++ tests/test_global_args.py
@@ -10,4 +10,21 @@ def test_connect(capfd, datadir):
         f'python {datadir/"global_args.py"} --as {user} -p {password} connect {hostname}',
         shell=True
     )
-    capfd.readouterr().out == f'Connecting to {hostname}, user="{user}", password="{password}"'
+
+    output = capfd.readouterr().out.strip()
+
+    assert output == f'Connecting to {hostname}, user="{user}", password="{password}"'
+
+def test_upload(capfd, datadir):
+    user = 'user'
+    password = 'password'
+    filename = 'filename'
+
+    run(
+        f'python {datadir/"global_args.py"} --as {user} -p {password} utils upload {filename}',
+        shell=True
+    )
+
+    output = capfd.readouterr().out.strip()
+
+    assert output == f'Uploading {filename}, user="{user}", password="{password}"'
