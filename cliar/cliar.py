90d7722e5b5b6787a91bfae1e01a9c9ec85dce32
Constantine Molchanov <moigagoo@live.com>
2019-08-04 16:43:56 +0400

    Add space.

--- cliar/cliar.py
+++ cliar/cliar.py
@@ -6,7 +6,7 @@ from typing import List, Iterable, Callable, Set, Type, get_type_hints
 from .utils import ignore
 
 
-# pylint: disable=too-few-public-methods,protected-access, too-many-instance-attributes
+# pylint: disable=too-few-public-methods, protected-access, too-many-instance-attributes
 
 
 class _Arg:
