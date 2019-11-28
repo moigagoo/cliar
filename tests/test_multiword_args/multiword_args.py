b5a9311764017e799c75c54b9bc92c6eaeb176f8
Constantine Molchanov <moigagoo@live.com>
2019-11-29 00:14:58 +0400

    Add test for multiword arg of non-boolean type.

--- tests/test_multiword_args/multiword_args.py
+++ tests/test_multiword_args/multiword_args.py
@@ -2,11 +2,12 @@ from cliar import Cliar
 
 
 class MultiwordArgs(Cliar):
-    def say(self, words_to_say: str, to_upper=False):
-        if to_upper:
-            print(words_to_say.upper())
-        else:
-            print(words_to_say)
+    def say(self, words_to_say: str, to_upper=False, repeat_words: int = 1):
+        for _ in range(repeat_words):
+            if to_upper:
+                print(words_to_say.upper())
+            else:
+                print(words_to_say)
 
 if __name__ == "__main__":
     MultiwordArgs().parse()
